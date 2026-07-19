3ebee47fb88a9b6c5f525735929d043f42292047
williams2558 <alberttuye@icloud.com>
2026-07-19 04:01:21 +0200

    Update generateAvatar.js
    Signed-off-by: williams2558 <alberttuye@icloud.com>

--- inconnu/generateAvatar.js
+++ inconnu/generateAvatar.js
@@ -1,6 +1,6 @@
 
 /**
- * INCONNU BOY TECH BOT
+ * 𝔎𝔦𝔫𝔤 𝔞𝔰𝔪𝔬𝔡𝔢𝔢 BOT
  * @param {string} name - Le nom ou identifiant utilisé comme "seed"
  * @param {string} style - Le style d'avatar (ex: "pixel", "bottts", "adventurer")
  * @returns {string} URL de l’avatar SVG généré
